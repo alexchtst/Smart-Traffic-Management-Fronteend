73f01751e5dfdc53c363cb9515b4fae07cb7d2ac
alexchtst-01 <acinatra@gmail.com>
2025-02-27 15:13:25 +0700

    feat: VehicleCounting initial folder space

--- src/components/main-layout.jsx
+++ src/components/main-layout.jsx
@@ -3,8 +3,8 @@ import { CustomMenuBar } from './menu-bar'
 
 const MainLayout = ({children}) => {
   return (
-    <div>
-        <div className='w-full flex items-center justify-center py-[3vw]'>
+    <div className='relative w-full h-full'>
+        <div className='w-full flex items-center justify-center py-[3vw] absolute z-[99]'>
             <CustomMenuBar />
         </div>
         <div>{children}</div>
