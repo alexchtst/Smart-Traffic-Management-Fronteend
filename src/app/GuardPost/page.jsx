07c3bfef8f25b5ab461ddb1bb87530f55e0bdee5
alexchtst-01 <acinatra@gmail.com>
2025-02-28 17:00:28 +0700

    feat: add guard post layouting page

--- src/app/GuardPost/page.jsx
+++ src/app/GuardPost/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client'
+
+import { CrossRoads } from '@/components/crossroads-camera';
+import { GuardList } from '@/components/guard-list';
+import React from 'react';
 
 const Page = () => {
   return (
-    <div>GuardPost</div>
-  )
-}
+    <div className='w-full min-h-screen space-y-5 md:space-y-0 md:flex md:h-screen'>
+      <div className='md:w-[65%] h-[80vw] md:h-screen p-[2vw]'>
+        <CrossRoads />
+      </div>
+      <div className='md:w-[35%] p-[2vw]'>
+        <GuardList />
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
