3c2fdf179f1410ca3249986ec7e25e36c5dfdee3
alexchtst-01 <acinatra@gmail.com>
2025-02-28 18:48:06 +0700

    feat: fixing deployment build

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,15 +1,15 @@
+// try everything here
+
 'use client'
 
 import React from 'react'
 import Map from '@/components/map'
 import { CardWithForm } from '@/components/card-form'
-import { Component } from '@/components/chart'
 
 const Page = () => {
   return (
     <div className='h-screen flex justify-center items-center'>
         <CardWithForm />
-        <Component />
     </div>
   )
 }
