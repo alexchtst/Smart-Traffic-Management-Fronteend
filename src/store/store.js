75e55848ee30c4376efa08d26e0d0c3732846dc0
alexchtst-01 <acinatra@gmail.com>
2025-03-02 19:52:38 +0700

    feat: reducer fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import exampleReducer from "./features/exampleSlice"
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      examle: exampleReducer
+    },
   })
 }
