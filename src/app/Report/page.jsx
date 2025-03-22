03e6df211973f6bafd36f625cf5b84f0f12be0eb
alexchtst-01 <acinatra@gmail.com>
2025-03-22 13:12:44 +0700

    delete the device table in reports because unescesary

--- src/app/Report/page.jsx
+++ src/app/Report/page.jsx
@@ -4,11 +4,8 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-full h-screen py-[15vw] px-[3vw] space-x-0 space-y-10 md:flex md:py-[8vw] md:space-y-0 md:space-x-10">
-      <div className="md:w-[40%]">
-        <RegionDeviceTable />
-      </div>
-      <div className="md:w-[60%]">
+    <div className="w-full h-fit py-[15vw] px-[3vw] space-x-0 space-y-10 md:flex md:py-[8vw] md:space-y-0 md:space-x-10">
+      <div className="md:w-[100%]">
         <ReportsTemplate />
       </div>
     </div>
--- src/components/reports-template.jsx
+++ src/components/reports-template.jsx
@@ -1,5 +1,6 @@
-'use client'
+"use client";
 
+import { Separator } from "@radix-ui/react-menubar";
 import { CrowdChart } from "./chart";
 import {
   Card,
@@ -12,7 +13,51 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 
+import { useState, useEffect } from "react";
+import { Button } from "./ui/button";
+
 export function ReportsTemplate() {
+  const [points, setPoints] = useState([
+    {
+      name: "poin 1",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "poin 2",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "poin 3",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "poin 4",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+  ]);
+  const [additionalNotes, setAdditionalNotes] = useState([
+    {
+      name: "notes 1",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "notes 2",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "notes 3",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+    {
+      name: "notes 4",
+      content: "Laboris do aliqua eiusmod esse culpa excepteur.",
+    },
+  ]);
+
+  const [summary, setSummary] = useState(
+    "Cupidatat sit veniam elit duis et mollit eu. Aliqua esse nostrud veniam occaecat minim laboris tempor sunt aliqua aliqua Lorem. Minim irure incididunt cupidatat magna id exercitation non aliqua. Fugiat ullamco elit id sint reprehenderit duis eu consequat veniam magna sunt pariatur tempor enim. Nisi elit consequat irure in aliquip ea nulla in proident consectetur."
+  );
+
   return (
     <Card>
       <CardHeader>
@@ -24,35 +69,59 @@ export function ReportsTemplate() {
       </CardHeader>
       <CardContent>
         <form type="submit">
-          <div className="space-y-5">
-            {/* title */}
-            <div>
-              <Label htmlFor="title">Title</Label>
-              <Input type="text" id="title" placeholder="Title" />
-            </div>
-            
-            {/* chart */}
-            <div>
-              <Label htmlFor="chart">This Data is not Editable**</Label>
-              <CrowdChart />
-            </div>
+          <div className="grid  md:grid-cols-2 grid-cols-1 gap-4">
+            {/* right */}
+            <div className="space-y-5">
+              {/* title */}
+              <div>
+                <Label htmlFor="title">Title</Label>
+                <Input type="text" id="title" placeholder="Title" />
+              </div>
 
-            {/* additional notes */}
-            <div>
-              <Label htmlFor="points">Points</Label>
-              <Input id="points" placeholder="Type your message here." />
-            </div>
+              {/* points */}
+              <div>
+                {points.map((point, idx) => (
+                  <div key={idx}>
+                    <Label htmlFor={point.name}>{point.name}</Label>
+                    <Input id={point.name} placeholder={point.content} />
+                  </div>
+                ))}
+                <Button className="my-2 w-full">Add Points</Button>
+              </div>
 
-            {/* additional notes */}
-            <div>
-              <Label htmlFor="notes">Additional Notes</Label>
-              <Input id="notes" placeholder="Type your message here." />
+              {/* additional notes */}
+              <div>
+                {additionalNotes.map((additionalNote, idx) => (
+                  <div key={idx}>
+                    <Label htmlFor={additionalNote.name}>
+                      {additionalNote.name}
+                    </Label>
+                    <Input
+                      id={additionalNote.name}
+                      placeholder={additionalNote.content}
+                    />
+                  </div>
+                ))}
+                <Button className="my-2 w-full">Add Points</Button>
+              </div>
             </div>
+            {/* left */}
+            <div className="space-y-5">
+              {/* chart */}
+              <div>
+                <Label htmlFor="chart">This Data is not Editable**</Label>
+                <CrowdChart />
+              </div>
 
-            {/* summary */}
-            <div>
-              <Label htmlFor="summary">Summary</Label>
-              <Textarea id="summary" placeholder="Type your message here." />
+              {/* summary */}
+              <div>
+                <Label htmlFor="summary">Summary</Label>
+                <Textarea
+                  id="summary"
+                  placeholder={summary}
+                  className="h-fit"
+                />
+              </div>
             </div>
           </div>
         </form>
